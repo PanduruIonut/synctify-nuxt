4702355c4cc2b2f7a066aebf650107c0a2aac997
Ionut Panduru <panduru.ionut@hotmail.com>
2023-10-11 00:00:03 +0300

    feat: Add toastify & use it when saving user settings & when synchronizing playlist

--- pages/boarding.vue
+++ pages/boarding.vue
@@ -1,52 +1,69 @@
 <script setup lang="ts">
-import { UserSettings } from '@/types/user'
-import { useUser } from '@/stores/user'
+import { UserSettings } from "@/types/user";
+import { useUser } from "@/stores/user";
+import { toast } from "vue3-toastify";
 
-const store = useUser()
+const store = useUser();
 const userSettings = ref<UserSettings>({
-    clientId: store.user.settings.clientId,
-    clientSecret: store.user.settings.clientSecret,
-    redirectUri: store.user.settings.redirectUri,
-})
+  clientId: store.user.settings.clientId,
+  clientSecret: store.user.settings.clientSecret,
+  redirectUri: store.user.settings.redirectUri,
+});
 
 function saveUserSettings() {
+  try {
     store.saveSettings(userSettings.value);
+    toast.success("Settings saved successfully!", {
+      position: toast.POSITION.TOP_CENTER,
+    });
+  } catch (error) {
+    toast.error("An error occurred while saving your settings", {
+      position: toast.POSITION.TOP_CENTER,
+    });
+  }
 }
-
 </script>
 
 <style scoped lang="scss">
 .boarding-card {
-    text-align: center;
+  text-align: center;
 
-    &__text-fields {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
+  &__text-fields {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 
-        &-input {
-            width: 350px;
-        }
+    &-input {
+      width: 350px;
     }
+  }
 }
 </style>
 
 <template>
-    <v-card class="boarding-card">
-        <v-card-title>
-            Welcome!
-        </v-card-title>
-        <v-card-text>
-            Before continuing you need to setup your spotify api
-        </v-card-text>
-        <div class="boarding-card__text-fields">
-            <v-text-field class="boarding-card__text-fields-input" label="client id"
-                v-model="userSettings.clientId"></v-text-field>
-            <v-text-field class="boarding-card__text-fields-input" label="client secret"
-                v-model="userSettings.clientSecret"></v-text-field>
-            <v-text-field class="boarding-card__text-fields-input" label="redirect uri"
-                v-model="userSettings.redirectUri"></v-text-field>
-        </div>
-        <v-btn @click="saveUserSettings()">Save</v-btn>
-</v-card></template>
+  <v-card class="boarding-card">
+    <v-card-title> Welcome! </v-card-title>
+    <v-card-text>
+      Before continuing you need to setup your spotify api
+    </v-card-text>
+    <div class="boarding-card__text-fields">
+      <v-text-field
+        class="boarding-card__text-fields-input"
+        label="client id"
+        v-model="userSettings.clientId"
+      ></v-text-field>
+      <v-text-field
+        class="boarding-card__text-fields-input"
+        label="client secret"
+        v-model="userSettings.clientSecret"
+      ></v-text-field>
+      <v-text-field
+        class="boarding-card__text-fields-input"
+        label="redirect uri"
+        v-model="userSettings.redirectUri"
+      ></v-text-field>
+    </div>
+    <v-btn @click="saveUserSettings()">Save</v-btn>
+  </v-card>
+</template>
--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,5 +1,51 @@
+<script setup lang="ts">
+import { useUser } from "@/stores/user";
+import { toast } from "vue3-toastify";
+const store = useUser();
+
+const syncPlaylist = () => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const response = await fetch("http://localhost:8000/create_playlist", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ access_token: store.user.accessToken }),
+      });
+
+      if (!response.ok) {
+        reject(
+          new Error(`Failed to create playlist. Status: ${response.status}`)
+        );
+        return;
+      }
+
+      const data = await response.json();
+      resolve(data);
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
+const sync = () => {
+  toast.promise(
+    syncPlaylist,
+    {
+      pending: "Synchronizing your liked songs ⏳︎",
+      success: "Playlist synchronized 👌",
+      error: "Error while synchronizing 🤯",
+    },
+    {
+      position: toast.POSITION.TOP_CENTER,
+    }
+  );
+};
+</script>
 <template>
-    <div>
-        Welcome to your profile!
-    </div>
+  <div>
+    <h1>Welcome to your profile!</h1>
+    <v-btn @click="sync">Sync your liked songs </v-btn>
+  </div>
 </template>
