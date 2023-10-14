c4a97eee2a67fc886965dd0f5e501306e575a14a
Ionut Panduru <panduru.ionut@hotmail.com>
2023-10-14 12:10:10 +0300

    fix: Index page handle redirects & hydratation error

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
 import { useUser } from '@/stores/user'
+import { useRouter } from 'vue-router'
 
 const store = useUser()
 const router = useRouter()
+
 const settings = !Object.values(store.user.settings).some(value => !value);
-if(settings){
-    store.user.accessToken ? router.push('/dashboard') : router.push('/authorize')
+
+if (process.client) {
+    if (settings) {
+        store.user.accessToken ? router.push('/dashboard') : router.push('/authorize')
+    } else {
+        router.push('/boarding')
+    }
 }
 </script>
+<template></template>
